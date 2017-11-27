2222ccd6a9d410f13c65865ef62b28ea41b1c32b
SerRod <rodn16@yahoo.com>
2017-11-27 22:46:06 +0200

    message

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 ln -s /home/box/web/hello.py /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+#sudo /etc/init.d/mysql start
 sudo gunicorn -c /home/box/web/etc/hello.py hello:app
