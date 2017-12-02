6505dbfee222ffb149498ea7f0aaa6d01720a812
SerRod <rodn16@yahoo.com>
2017-12-02 20:43:03 +0200

    first commit

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo rm /etc/nginx/sites-enabled/defaultt
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
