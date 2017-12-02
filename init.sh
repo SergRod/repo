7751ed68a3f0a9a43ac480590da205bddda214f7
SerRod <rodn16@yahoo.com>
2017-12-02 21:00:14 +0200

    first commit

--- init.sh
+++ init.sh
@@ -3,8 +3,7 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/test
-sudo gunicorn -c /home/box/web/etc/hello.py hello:app
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+
 
