aa3fc6d95f93466105acb1668dbaacb79c464232
SerRod <rodn16@yahoo.com>
2017-12-04 17:59:53 +0200

    message

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-#sudo rm /etc/nginx/sites-enabled/default
-#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-#sudo /etc/init.d/nginx restart
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
