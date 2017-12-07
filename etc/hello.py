e2012d4874c743f6d158758ab2eca0fd75ab5102
SerRod <rodn16@yahoo.com>
2017-12-07 10:02:04 +0200

    message

--- etc/hello.py
+++ etc/hello.py
@@ -1,11 +1,12 @@
 CONFIG = {
     'mode': 'wsgi',
     'working_dir': '/home/box/web/etc/',
+    'python': '/usr/bin/python3', 
     'args': (
         '--bind=0.0.0.0:8080',
         '--workers=4',
         '--timeout=60',
-        'hello:app'
+        'gunicorn -c hello.py hello:app'
     )
 }                                                                            
                                                                                                                                    
